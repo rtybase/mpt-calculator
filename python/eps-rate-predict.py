bcf8af5de8cfa57f667a772a3913b4062c39143b
rtybase <ruslan.ciurca@gmail.com>
2025-08-25 15:42:14 +0100

    minor improvements to the python code

--- python/eps-rate-predict.py
+++ python/eps-rate-predict.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy
 import util.ml
 
-numpy.set_printoptions(suppress = True, threshold = sys.maxsize)
-
 def predict_with_all_models(X, dataset_index):
     print("ds=%s after EPS, linear prediction=%s" % (dataset_index,\
         util.ml.predict_l(X, dataset_index)))
@@ -19,9 +17,16 @@ def predict_with_all_models(X, dataset_index):
         print("ds=%s after EPS, polynomial d=%s prediction=%s" % (dataset_index, degree,\
             util.ml.predict_p(degree, X, dataset_index)))
 
+numpy.set_printoptions(suppress=True, threshold=sys.maxsize, linewidth=500)
+
 if len(sys.argv) > 1:
     file = sys.argv[1]
     dataset = pd.read_csv(file)
+
+    assets = dataset['asset_id'].tolist()
+    print("assets=%s" % (assets))
+    print("==============================================")
+
     X = dataset[[*util.ml.CORE_COLUMNS_FOR_TRAINING, 'next_rate']].values
     predict_with_all_models(X, 2)
 
--- python/eps-rate-validate.py
+++ python/eps-rate-validate.py
@@ -2,16 +2,19 @@ import sys
 import numpy
 import util.ml
 from sklearn.metrics import r2_score
+from statistics import correlation
 
 def compute_matches(y_actual, y_predicted):
     return numpy.linalg.norm(y_actual - y_predicted)
 
 def report_model_details(y_actual, y_predicted, dataset_index, model_name):
     r2 = r2_score(y_actual, y_predicted)
+    corr = correlation(y_actual, y_predicted)
     print("---------------------------")
     print(f"ds={dataset_index} after EPS, {model_name} distance=%s" %\
         (compute_matches(y_actual, y_predicted)))
     print(f"ds={dataset_index} after EPS, {model_name} r2=%s" % (r2))
+    print(f"ds={dataset_index} after EPS, {model_name} corr=%s" % (corr))
 
 
 def validate_all_models(X,y_actual, dataset_index):
@@ -29,10 +32,12 @@ def validate_all_models(X,y_actual, dataset_index):
 
         y_predicted = util.ml.predict_p(degree, X, dataset_index)
         r2 = r2_score(y_actual, y_predicted)
+        corr = correlation(y_actual, y_predicted)
 
         print("ds=%s after EPS, polynomial d=%s distance=%s" % (dataset_index, degree,\
             compute_matches(y_actual, y_predicted)))
         print("ds=%s after EPS, polynomial d=%s r2=%s" % (dataset_index, degree, r2))
+        print("ds=%s after EPS, polynomial d=%s corr=%s" % (dataset_index, degree, corr))
 
     print("==============================================")
 
