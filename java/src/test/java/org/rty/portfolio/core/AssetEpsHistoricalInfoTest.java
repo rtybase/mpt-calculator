f90ef5511fcdb9b8bb3d07e01c639852550058fc
rtybase <ruslan.ciurca@gmail.com>
2025-10-03 18:51:27 +0100

    adding tests for AssetEpsHistoricalInfo

---
+++ java/src/test/java/org/rty/portfolio/core/AssetEpsHistoricalInfoTest.java
@@ -0,0 +1,355 @@
+package org.rty.portfolio.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+class AssetEpsHistoricalInfoTest {
+	private static final AssetNonGaapEpsInfo WITH_AMC = newNonGaapEpsInfo(0D, null, true);
+	private static final AssetNonGaapEpsInfo WITH_BMO = newNonGaapEpsInfo(0D, null, false);
+
+	protected static final Date D_2025_07_17 = new Date(125, 6, 17);
+	protected static final String TEST_ASSET = "MSFT";
+	protected static final double ERROR_TOLERANCE = 0.00001D;
+
+	@Test
+	void testGetMonthIndex() {
+		final AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0D, 0D), null, null,
+				null, null, null, null, null, null, null, null);
+
+		assertEquals(6, info.getMonthIndex());
+	}
+
+	@Test
+	void testGetAfterMarketCloseWithNullData() {
+		final AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, null, null, null,
+				null, null, null, null, null, null);
+
+		assertEquals(1, info.getPreviousAfterMarketClose());
+		assertEquals(1, info.getCurrentAfterMarketClose());
+	}
+
+	@Test
+	void testGetPreviousAfterMarketClose() {
+		final AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, null,
+				newNonGaapEpsInfo(0D, 0D, false), null, null, null, null, null, null, null);
+
+		assertEquals(0, info.getPreviousAfterMarketClose());
+		assertEquals(1, info.getCurrentAfterMarketClose());
+	}
+
+	@Test
+	void testGetCurrentAfterMarketClose() {
+		final AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null,
+				newNonGaapEpsInfo(0D, 0D, false), null, null, null, null, null, null, null, null, null);
+
+		assertEquals(1, info.getPreviousAfterMarketClose());
+		assertEquals(0, info.getCurrentAfterMarketClose());
+	}
+
+	@Test
+	void tesGetPreviousPredictedEps() {
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0D, 1D),
+				newNonGaapEpsInfo(0D, 2D, false), null, null, null, null, null, null, null);
+
+		assertEquals(1D, info.getPreviousPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0D, null),
+				newNonGaapEpsInfo(0D, 2D, false), null, null, null, null, null, null, null);
+
+		assertEquals(2D, info.getPreviousPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0.5D, null), WITH_BMO, null, null,
+				null, null, null, null, null);
+
+		assertEquals(0.5D, info.getPreviousPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0.5D, null), null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(0.5D, info.getPreviousPredictedEps(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void tesGetCurrentPredictedEps() {
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0D, 1D),
+				newNonGaapEpsInfo(0D, 2D, false), null, null, null, null, null, null, null, null, null);
+
+		assertEquals(1D, info.getCurrentPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0D, null), newNonGaapEpsInfo(0D, 2D, false),
+				null, null, null, null, null, null, null, null, null);
+
+		assertEquals(2D, info.getCurrentPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0.5D, null), WITH_BMO, null, null, null, null,
+				null, null, null, null, null);
+
+		assertEquals(0.5D, info.getCurrentPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0.5D, null), null, null, null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(0.5D, info.getCurrentPredictedEps(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetPreviousNonGaapPredictedEps() {
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0D, 1D),
+				newNonGaapEpsInfo(0D, 2D, false), null, null, null, null, null, null, null);
+
+		assertEquals(2D, info.getPreviousNonGaapPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0D, 1D), WITH_BMO, null, null, null,
+				null, null, null, null);
+
+		assertEquals(1D, info.getPreviousNonGaapPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0D, null),
+				newNonGaapEpsInfo(0.5D, null, false), null, null, null, null, null, null, null);
+
+		assertEquals(0.5D, info.getPreviousNonGaapPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(0.5D, null), null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(0.5D, info.getPreviousNonGaapPredictedEps(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetCurrentNonGaapPredictedEps() {
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0D, 1D),
+				newNonGaapEpsInfo(0D, 2D, false), null, null, null, null, null, null, null, null, null);
+
+		assertEquals(2D, info.getCurrentNonGaapPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0D, 1D), WITH_BMO, null, null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(1D, info.getCurrentNonGaapPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0D, null), newNonGaapEpsInfo(0.5D, null, false),
+				null, null, null, null, null, null, null, null, null);
+
+		assertEquals(0.5D, info.getCurrentNonGaapPredictedEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(0.5D, null), null, null, null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(0.5D, info.getCurrentNonGaapPredictedEps(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetPreviousNonGaapEps() {
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(1D, null),
+				newNonGaapEpsInfo(2D, null, false), null, null, null, null, null, null, null);
+
+		assertEquals(2D, info.getPreviousNonGaapEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(1D, null), null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(1D, info.getPreviousNonGaapEps(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetCurrentNonGaapEps() {
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(1D, null),
+				newNonGaapEpsInfo(2D, null, false), null, null, null, null, null, null, null, null, null);
+
+		assertEquals(2D, info.getCurrentNonGaapEps(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(1D, null), null, null, null, null, null, null,
+				null, null, null, null);
+
+		assertEquals(1D, info.getCurrentNonGaapEps(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetPreviousPOverE() {
+		final AssetPriceInfo beforePrevious = newPriceInfo(2D);
+		final AssetPriceInfo previous = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(1D, null),
+				WITH_BMO, previous, beforePrevious, null, null, null, null, null);
+
+		assertEquals(2D, info.getPreviousPOverE(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(1D, null), WITH_AMC, previous,
+				beforePrevious, null, null, null, null, null);
+
+		assertEquals(1D, info.getPreviousPOverE(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, newEpsInfo(1D, null), WITH_BMO, previous, null,
+				null, null, null, null, null);
+
+		assertEquals(1D, info.getPreviousPOverE(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetCurrentPOverE() {
+		final AssetPriceInfo beforeCurrent = newPriceInfo(2D);
+		final AssetPriceInfo current = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(1D, null), WITH_BMO, null,
+				null, null, null, null, beforeCurrent, current, null, null);
+
+		assertEquals(2D, info.getCurrentPOverE(), ERROR_TOLERANCE);
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, newEpsInfo(1D, null), WITH_AMC, null, null, null, null,
+				null, beforeCurrent, current, null, null);
+
+		assertEquals(1D, info.getCurrentPOverE(), ERROR_TOLERANCE);
+	}
+
+	@Test
+	void testGetInfoBeforeMinusOneDayEpsAnnouncement() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, forBmo, forAmc, null, null, null);
+
+		assertSame(forBmo, info.getInfoBeforeMinusOneDayEpsAnnouncement());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, forBmo, forAmc,
+				null, null, null);
+
+		assertSame(forAmc, info.getInfoBeforeMinusOneDayEpsAnnouncement());
+	}
+
+	@Test
+	void testGetInfoBeforeEpsAnnouncement() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, null, forBmo, forAmc, null, null);
+
+		assertSame(forBmo, info.getInfoBeforeEpsAnnouncement());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, null, forBmo,
+				forAmc, null, null);
+
+		assertSame(forAmc, info.getInfoBeforeEpsAnnouncement());
+	}
+
+	@Test
+	void testGetInfoAfterEpsAnnouncement() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, null, null, forBmo, forAmc, null);
+
+		assertSame(forBmo, info.getInfoAfterEpsAnnouncement());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, null, null, forBmo,
+				forAmc, null);
+
+		assertSame(forAmc, info.getInfoAfterEpsAnnouncement());
+	}
+
+	@Test
+	void testGetInfoAfterPlusOneDayEpsAnnouncement() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, null, null, null, forBmo, forAmc);
+
+		assertSame(forBmo, info.getInfoAfterPlusOneDayEpsAnnouncement());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, null, null, null,
+				forBmo, forAmc);
+
+		assertSame(forAmc, info.getInfoAfterPlusOneDayEpsAnnouncement());
+	}
+
+	@Test
+	void testIsGoodForNothing() {
+		final AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, null, null, null, null,
+				null, null, null, null, null, null);
+
+		assertFalse(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertFalse(info.isGoodForAfterEpsTraining());
+		assertFalse(info.isGoodForAfterEpsPrediction());
+	}
+
+	@Test
+	void testIsGoodForAfterPlusOneDayEpsTraining() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, forBmo, forBmo, forBmo, forBmo, null);
+
+		assertTrue(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertTrue(info.isGoodForAfterEpsTraining());
+		assertTrue(info.isGoodForAfterEpsPrediction());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, null, forAmc,
+				forAmc, forAmc, forAmc);
+
+		assertTrue(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertTrue(info.isGoodForAfterEpsTraining());
+		assertTrue(info.isGoodForAfterEpsPrediction());
+	}
+
+	@Test
+	void testIsGoodForAfterEpsTraining() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, forBmo, forBmo, forBmo, null, null);
+
+		assertFalse(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertTrue(info.isGoodForAfterEpsTraining());
+		assertTrue(info.isGoodForAfterEpsPrediction());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, null, forAmc,
+				forAmc, forAmc, null);
+
+		assertFalse(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertTrue(info.isGoodForAfterEpsTraining());
+		assertTrue(info.isGoodForAfterEpsPrediction());
+	}
+
+	@Test
+	void testIsGoodForAfterEpsPrediction() {
+		final AssetPriceInfo forBmo = newPriceInfo(2D);
+		final AssetPriceInfo forAmc = newPriceInfo(1D);
+
+		AssetEpsHistoricalInfo info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_BMO, null, null, null,
+				null, forBmo, forBmo, null, null, null);
+
+		assertFalse(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertFalse(info.isGoodForAfterEpsTraining());
+		assertTrue(info.isGoodForAfterEpsPrediction());
+
+		info = new AssetEpsHistoricalInfo(TEST_ASSET, 1, 1, null, WITH_AMC, null, null, null, null, null, forAmc,
+				forAmc, null, null);
+
+		assertFalse(info.isGoodForAfterPlusOneDayEpsTraining());
+		assertFalse(info.isGoodForAfterEpsTraining());
+		assertTrue(info.isGoodForAfterEpsPrediction());
+	}
+
+	private static AssetPriceInfo newPriceInfo(double price) {
+		return new AssetPriceInfo(TEST_ASSET, price, 0.1D, 1D, 10D, 1D, D_2025_07_17);
+	}
+
+	private static AssetEpsInfo newEpsInfo(double eps, Double predictedEps) {
+		return new AssetEpsInfo(TEST_ASSET, eps, predictedEps, D_2025_07_17);
+	}
+
+	private static AssetNonGaapEpsInfo newNonGaapEpsInfo(double eps, Double predictedEps, boolean afterMarketClose) {
+		return new AssetNonGaapEpsInfo(TEST_ASSET, eps, predictedEps, afterMarketClose, 1D, 1D, D_2025_07_17);
+	}
+}
