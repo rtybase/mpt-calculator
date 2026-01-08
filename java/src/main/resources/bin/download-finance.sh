84ee54892f8590e97a19af33da0395d849cd2948
rtybase <ruslan.ciurca@gmail.com>
2026-01-08 18:12:39 +0000

    extracting more financial data for ML

---
+++ java/src/main/resources/bin/download-finance.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -o pipefail
+set -ue
+
+FOLDER_FOR_FINANCE_FILES=${FOLDER_FOR_FINANCE_FILES:-"./data_to_load_finance"}
+
+load_fscore () {
+	ticker=$1
+	echo "---------------------------------------------------"
+	echo "Finance data for: ${ticker}"
+
+	out_file_name=`echo "$1" | sed -e 's/[\.\%]/-/g' | tr '[:upper:]' '[:lower:]'`;
+	finance_out_file="${FOLDER_FOR_FINANCE_FILES}/${out_file_name}.csv"
+
+	if [ -f $finance_out_file ]; then
+		echo "${finance_out_file} already exists."
+	else
+		python more-fin-data.py ${ticker} 1>>${finance_out_file} 2>/dev/null
+	fi
+}
+
+input_file=$1
+echo "Loading definitions from ${input_file}"
+
+mkdir -p ${FOLDER_FOR_FINANCE_FILES}
+
+dos2unix ${input_file}
+
+while IFS='=' read -r key value
+do
+#    echo "Key: [$key]"
+#    echo "Value: [$value]"
+	
+	load_fscore "$key" "$value" || true
+done < "${input_file}"
---
+++ java/src/main/resources/bin/more-fin-data.py
@@ -0,0 +1,77 @@
+import requests
+import sys
+import csv
+import pandas as pd
+import util.flow
+
+URL = "https://api.nasdaq.com/api/company/%s/financials?frequency=2"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+    "Origin": "https://www.nasdaq.com",
+    "Referer": "https://www.nasdaq.com/"
+}
+
+REPORT_VALUES = {
+    "balanceSheetTable": ["Total Current Assets", "Total Current Liabilities",
+                          "Total Liabilities", "Total Equity", "Total Assets"],
+    "cashFlowTable": ["Net Cash Flow-Operating", "Capital Expenditures"]
+}
+
+def add_colums_with_data_from_dict(from_dict, to_list):
+    if from_dict:
+        index = 0
+        for k in from_dict:
+            if (index >= len(to_list)):
+                to_list.append([])
+
+            to_list[index].append(from_dict[k])
+            index += 1
+
+def add_colums_with_data_from_list(from_list, to_list, col_name):
+    if from_list:
+        for dct in from_list:
+            if dct["value1"] == col_name:
+                add_colums_with_data_from_dict(dct, to_list)
+
+def load_fin_for(symbol, rows):
+    final_url = URL % (symbol,)
+    response = requests.get(final_url, headers=HEADERS)
+
+    if response.status_code == 200:
+        data = response.json()
+        try:
+            symbol_dct = {"value1": "Symbol", "value2": symbol, 
+                          "value3": symbol, "value4": symbol, "value5": symbol}
+
+            add_colums_with_data_from_dict(symbol_dct, rows)
+            add_colums_with_data_from_dict(data['data']['balanceSheetTable']['headers'],\
+                rows)
+
+            for k in REPORT_VALUES:
+                for v in REPORT_VALUES[k]:
+                    add_colums_with_data_from_list(data['data'][k]['rows'],\
+                        rows, v)
+
+        except KeyError:
+            print(data, flush=True)
+            util.flow.error("Unexpected JSON structure!")
+    else:
+        util.flow.error(f"Request for {final_url} failed with status code {response.status_code}")
+
+
+def print_data_as_csv(lst):
+    df = pd.DataFrame(lst)
+    df.to_csv(sys.stdout, index=False, header=False, encoding='utf-8')
+
+
+if len(sys.argv) > 1:
+    symbol = sys.argv[1]
+
+    rows = []
+    load_fin_for(symbol, rows)
+    print_data_as_csv(rows)
+
+else:
+    util.flow.error("Specify the symbol!")
