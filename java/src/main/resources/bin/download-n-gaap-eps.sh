12acea003232504283f3594bc6e9e2f2047adee3
rtybase <ruslan.ciurca@gmail.com>
2026-01-29 18:48:31 +0000

    added caching to the n-gaap eps data loader

--- java/src/main/resources/bin/download-n-gaap-eps.sh
+++ java/src/main/resources/bin/download-n-gaap-eps.sh
@@ -3,6 +3,8 @@ set -o pipefail
 set -ue
 
 FOLDER_FOR_N_GAAP_EPS_FILES=${FOLDER_FOR_N_GAAP_EPS_FILES:-"./data_to_load_n_gaap_eps"}
+CACHE_FOLDER="/d/cache"
+MAX_AGE_SECONDS=$((5*24*60*60))
 
 extract_session() {
 	java -Duse-http2=true -jar portfolio-0.0.1-SNAPSHOT.jar DownloadTask \
@@ -16,24 +18,48 @@ extract_session() {
 	rm -rf msft-ss-tt-yy.html
 }
 
+update_id() {
+	ticker=$1
+	out_file=$2
+
+	java -Duse-http2=true -jar portfolio-0.0.1-SNAPSHOT.jar DownloadTask \
+		"-url=https://api.investing.com/api/search/v2/search?q=$ticker" \
+		-outfile=$out_file -headers=headers/investing-sess.prop
+
+}
+
+update_id_if_required() {
+	ticker=$1
+	out_file=$2
+
+	if [ -f $out_file ]; then
+		file_age_is_seconds=$(($(date +%s) - $(date -r "${out_file}" +%s)))
+
+		if [[ $file_age_is_seconds -gt MAX_AGE_SECONDS ]]; then
+			update_id $ticker ${id_file}
+		fi
+	else
+		update_id $ticker ${id_file}
+	fi
+}
+
 load_n_gaap_eps () {
 	ticker=$1
 	asset_name=$2
 	echo "---------------------------------------------------"
 	echo "Non-GAAP EPS data for: ${asset_name}"
 
-	out_file_name=`echo "$1" | sed -e 's/[\.\%]/-/g' | tr '[:upper:]' '[:lower:]'`;
+	out_file_name=`echo "$ticker" | sed -e 's/[\.\%]/-/g' | tr '[:upper:]' '[:lower:]'`;
 	eps_out_file="${FOLDER_FOR_N_GAAP_EPS_FILES}/${out_file_name}.csv"
+	id_file="${CACHE_FOLDER}/${out_file_name}-id.json"
 
 	if [ -f $eps_out_file ]; then
 		echo "${eps_out_file} already exists."
-	else 
-		java -Duse-http2=true -jar portfolio-0.0.1-SNAPSHOT.jar DownloadTask \
-			"-url=https://api.investing.com/api/search/v2/search?q=$ticker" \
-			-outfile=s-id.json -headers=headers/investing-sess.prop
+	else
+		update_id_if_required $ticker ${id_file}
 
-		if [ -s s-id.json ]; then
-			symbol_id=`python to_csv_ngaap_eps.py s-id.json ${ticker}`
+		if [ -s "${id_file}" ]; then
+			symbol_id=`python to_csv_ngaap_eps.py ${id_file} ${ticker}`
 
 			java -Duse-http2=true -jar portfolio-0.0.1-SNAPSHOT.jar DownloadTask \
 				"-url=https://endpoints.investing.com/earnings/v1/instruments/$symbol_id/earnings?limit=10" \
@@ -51,8 +77,6 @@ load_n_gaap_eps () {
 		else
 			echo "No ID for ${asset_name}"
 		fi
-
-		rm -rf s-id.json
 	fi
 }
 
@@ -60,6 +84,7 @@ input_file=$1
 echo "Loading definitions from ${input_file}"
 
 mkdir -p ${FOLDER_FOR_N_GAAP_EPS_FILES}
+mkdir -p ${CACHE_FOLDER}
 
 dos2unix ${input_file}
 
