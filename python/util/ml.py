9bf08c6348d42a5a22b25aac53208633e716cf5c
rtybase <ruslan.ciurca@gmail.com>
2026-07-11 16:28:14 +0100

    minor improvements to the hyper params search

--- python/util/ml.py
+++ python/util/ml.py
@@ -18,7 +18,7 @@ DTR_DS2_ARGS = {'max_depth': 5, 'min_samples_leaf': 18,\
         'min_samples_split': 2, 'criterion': 'absolute_error',\
         'random_state': 42}
 
-DTR_DS1_ARGS = {'max_depth': 6, 'min_samples_leaf': 8,\
+DTR_DS1_ARGS = {'max_depth': 6, 'min_samples_leaf': 20,\
         'min_samples_split': 2, 'criterion': 'absolute_error',\
         'random_state': 42}
 
@@ -35,7 +35,7 @@ RFR_DS1_ARGS = {'max_depth': 5, 'min_samples_leaf': 18,\
 XGB_DS2_ARGS = {'booster': 'gbtree', 'eval_metric': 'rmse',\
         'reg_alpha': 8.54327702906688, 'reg_lambda': 7.960301462774691,\
         'colsample_bytree': 0.6, 'subsample': 0.9,\
-        'learning_rate': 0.03187866984798271, 'max_depth': 6,\
+        'learning_rate': 0.03187866984798271, 'max_depth': 5,\
         'min_child_weight': 5,\
         'n_estimators': 300, 'random_state': 42,\
         'n_jobs': -1, 'nthread': -1,\
@@ -46,8 +46,8 @@ XGB_DS1_ARGS = {'booster': 'gbtree', 'eval_metric': 'rmse',\
         'reg_alpha': 8.54327702906688, 'reg_lambda': 7.960301462774691,\
         'colsample_bytree': 0.6, 'subsample': 0.9,\
         'learning_rate': 0.03187866984798271, 'max_depth': 7,\
-        'min_child_weight': 5,\
-        'n_estimators': 300, 'random_state': 42,\
+        'min_child_weight': 4,\
+        'n_estimators': 350, 'random_state': 42,\
         'n_jobs': -1, 'nthread': -1,\
         'objective': 'reg:squarederror', 'tree_method': 'hist',\
         'device': 'cuda'}
